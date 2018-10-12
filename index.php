2ab1b3e5707b090c5254e4d317e6d51483c82907
Luis Mizuno <lmizuno@northern.co>
2018-10-12 09:59:41 -0400

    final setup

--- index.php
+++ index.php
@@ -5,3 +5,26 @@
  * Date: 10/12/2018
  * Time: 9:45 AM
  */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="externals/bootstrap.css">
+    <link href="externals/bootstrap-grid.css">
+    <link href="externals/bootstrap-reboot.min.css">
+    <link href="css/style.css" rel="stylesheet">
+    <title>My Php Project</title>
+</head>
+<body>
+
+
+<script src="externals/jq-min.js"></script>
+<script src="externals/bootstrap.bundle.js"></script>
+<script src="externals/bootstrap.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
